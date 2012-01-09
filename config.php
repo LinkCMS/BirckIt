af80a479774a055e54f82a0011c6502ed46eea1a
Maxim Klebanov <prostocyber@gmail.com>
2012-01-09 17:18:46 +0900

    Config Update

--- config.php
+++ config.php
@@ -1,3 +1,8 @@
 <?php
 define('MOD_DIR','./modules/');
+define('DB_HOST','localhost');
+define('DB_USER','LinkCMS');
+define('DB_PASS','');
+define('DB_NAME','LinkCMS');
+define('DB_CHARSET','utf8');
 ?>
